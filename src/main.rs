98b9edabf1148112b2a25c97a229d0ad74118f2e
Angus Chiu <anguschiu1@users.noreply.github.com>
2022-11-26 18:35:04 +0000

    separate functionality into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
-use std::{env, fs, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,35 +9,8 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        return Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        });
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
 }
